4f68c1fec4a06ff10f92df7824bcc87c37dbd362
Elise Kristiansen <elise.kri@hotmail.com>
2025-02-22 16:26:43 +0100

    Added the first page and a menu button

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener("click", function (event) {
     if (event.target.matches(".window__close-button")) {
-      console.log("test");
-
       const parentDiv = event.target.closest(".window");
       if (parentDiv) {
-        parentDiv.remove();
+        if (parentDiv.matches("#main-menu")) {
+          var popup = document.getElementById("main-menu");
+          popup.classList.toggle("show");
+        } else {
+          parentDiv.remove();
+        }
       }
+    }
+    if (event.target.matches("#menu-button")) {
+      var popup = document.getElementById("main-menu");
+      popup.classList.toggle("show");
     }
   });
 });
