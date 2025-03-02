8719068880bfb16c26a4642fcd5ed9268462dc74
Elise Kristiansen <elise.kri@hotmail.com>
2025-03-02 19:37:17 +0100

    Added a possum

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
       const parentDiv = target.closest(".window");
       if (parentDiv) {
         const id = parentDiv.id;
-        if (id === "main-menu" || id === "easter-egg" || id === "pizza-egg") {
+        if (
+          id === "main-menu" ||
+          id === "easter-egg" ||
+          id === "pizza-egg" ||
+          id === "possum"
+        ) {
           parentDiv.classList.toggle("show");
         } else {
           parentDiv.remove();
@@ -15,13 +20,19 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
 
-    if (target.matches("#menu-button, #easter-egg-button, #pizza-egg-button")) {
+    if (
+      target.matches(
+        "#menu-button, #easter-egg-button, #pizza-egg-button, #possum-button"
+      )
+    ) {
       const popupId =
         target.id === "menu-button"
           ? "main-menu"
           : target.id === "easter-egg-button"
           ? "easter-egg"
-          : "pizza-egg";
+          : target.id === "pizza-egg-button"
+          ? "pizza-egg"
+          : "possum";
       document.getElementById(popupId)?.classList.toggle("show");
     }
   });
